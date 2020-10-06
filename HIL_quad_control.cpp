d7dff6747701ba584d15ae34f0d88e419eb3f3ce
jules <Julian_blnc@yahoo.com>
2020-10-06 18:59:44 -0400

    HIL prgroess

---
+++ HIL_quad_control.cpp
@@ -0,0 +1,311 @@
+// Hardware in the Loop interface to control Quadcopter in the Gazebo simulation environment
+//
+// By: Patrick Ledzian, Julian Blanco
+// Date: 06 October 2020
+//
+
+#include "include/HIL_quad_control.hpp"
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
+
+
+ int globalfiledescriptor;
+ Eigen::Matrix<double,1,3> _euler;
+
+//// Subscriber callback functions
+// Provides measured motor velocity
+void rotor0_cb(MotorSpeedPtr &rotor_vel)
+{
+    sensor_motor_vel0 = static_cast<double>(rotor_vel->data());
+}
+
+void rotor1_cb(MotorSpeedPtr &rotor_vel)
+{
+    sensor_motor_vel1 = static_cast<double>(rotor_vel->data());
+}
+
+void rotor2_cb(MotorSpeedPtr &rotor_vel)
+{
+    sensor_motor_vel2 = static_cast<double>(rotor_vel->data());
+}
+
+void rotor3_cb(MotorSpeedPtr &rotor_vel)
+{
+    sensor_motor_vel3 = static_cast<double>(rotor_vel->data());
+}
+
+
+
+int set_interface_attribs (int fd, int speed, int parity)
+{
+        struct termios tty;
+        if (tcgetattr (fd, &tty) != 0)
+        {
+                // error_message ("error %d from tcgetattr", errno);
+                return -1;
+        }
+
+        cfsetospeed (&tty, speed);
+        cfsetispeed (&tty, speed);
+
+        tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
+        // disable IGNBRK for mismatched speed tests; otherwise receive break
+        // as \000 chars
+        tty.c_iflag &= ~IGNBRK;         // disable break processing
+        tty.c_lflag = 0;                // no signaling chars, no echo,
+                                        // no canonical processing
+        tty.c_oflag = 0;                // no remapping, no delays
+        tty.c_cc[VMIN]  = 0;            // read doesn't block
+        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
+
+        tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
+                                        // enable reading
+        tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
+        tty.c_cflag |= parity;
+        tty.c_cflag &= ~CSTOPB;
+        tty.c_cflag &= ~CRTSCTS;
+
+        if (tcsetattr (fd, TCSANOW, &tty) != 0)
+        {
+                // error_message ("error %d from tcsetattr", errno);
+                return -1;
+        }
+        return 0;
+}
+
+void set_blocking (int fd, int should_block){
+        struct termios tty;
+        memset (&tty, 0, sizeof tty);
+        // if (tcgetattr (fd, &tty) != 0)
+        // {
+        //         // error_message ("error %d from tggetattr", errno);
+        //         return;
+        // }
+
+        tty.c_cc[VMIN]  = should_block ? 1 : 0;
+        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+        // if (tcsetattr (fd, TCSANOW, &tty) != 0)
+                // error_message ("error %d setting term attributes", errno);
+}
+
+
+// TODO: not sure if "iris" is the same index every run, need to run a for loop to find it and its index
+// TODO: put in write lock here and read lock in derived_sensor_values()
+
+//// Subscriber callback function
+// pulls from Gazebo: sim time (seconds), linear position from origin (meters), and orientation (quaternion)
+void local_poses_cb(ConstLocalPosesStampedPtr &local_pose)
+{
+    char str1[200] = "<";
+    sim_time = local_pose->time().sec() + (local_pose->time().nsec() * 10E-10);
+
+    _sensor_pos(0) = local_pose->pose(0).position().x();
+    _sensor_pos(1) = local_pose->pose(0).position().y();
+    _sensor_pos(2) = local_pose->pose(0).position().z();
+
+    _sensor_quat(0) = local_pose->pose(0).orientation().w();
+    _sensor_quat(1) = local_pose->pose(0).orientation().x();
+    _sensor_quat(2) = local_pose->pose(0).orientation().y();
+    _sensor_quat(3) = local_pose->pose(0).orientation().z();
+    _euler = quat2euler(_sensor_quat);
+
+    int leng = snprintf(str1,sizeof(str1),"<%f,%f,%f,%f,%f,%f>",_sensor_pos(0),_sensor_pos(1),_sensor_pos(2),euler_(0),euler_(1),euler_(2));
+    write (globalfiledescriptor, str1, leng);
+
+} // end local_poses_cb()
+
+//// Main loop
+// First portion sets up the messaging with Gazebo and logging file handle
+// Second portion is the while(1) loop that runs for duration of quadcopter manuevers
+int main(int _argc, char **_argv)
+{
+
+    //set up HIL
+    const char* portname = "/dev/ttyACM4";
+
+    globalfiledescriptor = open (portname, O_RDWR | O_NOCTTY | O_SYNC);
+    if (globalfiledescriptor < 0)
+    {
+            // error_message ("error %d opening %s: %s", errno, portname, strerror (errno));
+            std::cout << "error" << std::endl;
+            return -1;
+    }
+
+    set_interface_attribs (globalfiledescriptor, B115200, 0);  // set speed to 115,200 bps, 8n1 (no parity)
+    set_blocking (globalfiledescriptor, 0);                // set no blocking
+    FILE* rwfile = fdopen(globalfiledescriptor,"rw");
+    char buf [100];
+    
+    
+    
+    // Load gazebo as a client
+    gazebo::client::setup(_argc, _argv);
+
+    // Create our node for communication
+    node_handle = gazebo::transport::NodePtr(new gazebo::transport::Node());
+    node_handle->Init();
+
+    // Publish motor reference commands
+    gazebo::transport::PublisherPtr pub0 =
+        node_handle->Advertise<std_msgs::msgs::Float>("/gazebo/default/iris/ref/motor_speed/0", 1);
+    gazebo::transport::PublisherPtr pub1 =
+        node_handle->Advertise<std_msgs::msgs::Float>("/gazebo/default/iris/ref/motor_speed/1", 1);
+    gazebo::transport::PublisherPtr pub2 =
+        node_handle->Advertise<std_msgs::msgs::Float>("/gazebo/default/iris/ref/motor_speed/2", 1);
+    gazebo::transport::PublisherPtr pub3 =
+        node_handle->Advertise<std_msgs::msgs::Float>("/gazebo/default/iris/ref/motor_speed/3", 1);
+
+    // Subscribe to measured position and orientation values
+    gazebo::transport::SubscriberPtr sub5 = node_handle->Subscribe("~/pose/local/info", local_poses_cb);
+
+
+    // open file handle for storing test data for later analysis
+    std::ofstream testdata;
+    testdata.open("test_data.txt");
+    u_int64_t ctr = 0;                  // timing counter, good for 2^64 bits (essentially infinite)
+
+    std::vector<int> vect;
+    int val;
+    std::stringstream ss;
+    std::string substr;
+
+    fgets (buf, sizeof(buf),rwfile); 
+    fgets (buf, sizeof(buf),rwfile); 
+    fgets (buf, sizeof(buf),rwfile); 
+    fgets (buf, sizeof(buf),rwfile); 
+    fgets (buf, sizeof(buf),rwfile); 
+    
+    while(1)
+    {
+        fgets (buf, sizeof(buf),rwfile);
+        ss.str(buf);
+        std::cout  <<ss.str() ; 
+        while( ss.good() )
+        {
+            
+            getline( ss, substr, ',' );
+            // std::cout<<substr;
+            vect.push_back( std::stoi(substr) );
+        }
+
+        ref_motor_vel0.set_data(vect[0]);
+        ref_motor_vel1.set_data(vect[1]);
+        ref_motor_vel2.set_data(vect[2]);
+        ref_motor_vel3.set_data(vect[3]);
+
+        pub0->Publish(ref_motor_vel0);
+        pub1->Publish(ref_motor_vel1);
+        pub2->Publish(ref_motor_vel2);
+        pub3->Publish(ref_motor_vel3);
+        vect.clear();
+        ss.clear();
+//        std::cout << "sensor motor 0: " << sensor_motor_vel0 << std::endl;
+//        std::cout << "sensor motor 1: " << sensor_motor_vel1 << std::endl;
+//        std::cout << "sensor motor 2: " << sensor_motor_vel2 << std::endl;
+//        std::cout << "sensor motor 3: " << sensor_motor_vel3 << std::endl;
+//        std::cout << std::endl;
+
+    } // end while(1)
+
+    return(0);
+
+} // end main()
+
+//// Derive sensor values from position and orientation measurements
+void derived_sensor_values()
+{
+    // Base clock defined here at 1MHz
+    if ((sim_state == 2) & ((sim_time - prev_sim_time) > 0.000001))                      // currently running at the micro-second scale
+    {
+        // TODO: sometimes I get a 0.0 evaluation for lin_vel_z between otherwise good values, not sure why
+        sim_time_delta = sim_time - prev_sim_time;              // time slice used to derive velocity values
+
+        _derived_lin_vel(0) = (_sensor_pos(0) - _prev_sensor_pos(0)) / sim_time_delta;
+        _derived_lin_vel(1) = (_sensor_pos(1) - _prev_sensor_pos(1)) / sim_time_delta;
+        _derived_lin_vel(2) = (_sensor_pos(2) - _prev_sensor_pos(2)) / sim_time_delta;
+
+        Eigen::Matrix<double,1,3> euler_ = quat2euler(_sensor_quat);        // convert from quaternion to euler angles
+
+        _derived_pqr_att = derive_ang_velocity(euler_);                     // produces angular velocity vector
+
+        // store previous position value
+        _prev_sensor_pos(0) = _sensor_pos(0);
+        _prev_sensor_pos(1) = _sensor_pos(1);
+        _prev_sensor_pos(2) = _sensor_pos(2);
+
+        // store previous attitude value
+        _prev_derived_euler_att(0) = euler_(0);
+        _prev_derived_euler_att(1) = euler_(1);
+        _prev_derived_euler_att(2) = euler_(2);
+
+        prev_sim_time = sim_time;                   // should be the last thing run
+
+    } // end if(sim_state == 2 and 1MHz clock cycle)
+} // end derived_sensor_values()
+
+
+//
+// HELPER FUNCTIONS
+//
+
+// References
+// https://stackoverflow.com/questions/28585653/use-of-lpnorm-in-eigen
+// https://eigen.tuxfamily.org/dox/TopicFunctionTakingEigenTypes.html
+// https://stackoverflow.com/questions/35612831/eigenref-in-pass-by-pointer
+
+//// Convert quaternions to a rotation matrix
+// used in quat2euler()
+Eigen::Matrix3d quat2rot(const Eigen::Ref<const Eigen::Matrix<double,1,4>>& q_)
+{
+    Eigen::Matrix3d rotation_;
+
+    _quat_normalized = q_.normalized();
+    _q_hat(0,0) = 0.0;                            _q_hat(0,1) = -1.0*_quat_normalized(3); _q_hat(0,2) = _quat_normalized(2);
+    _q_hat(1,0) = _quat_normalized(3);      _q_hat(1,1) = 0.0;                            _q_hat(1,2) = -1.0*_quat_normalized(1);
+    _q_hat(2,0) = -1.0*_quat_normalized(2); _q_hat(2,1) = _quat_normalized(1);      _q_hat(2,2) = 0.0;
+
+    rotation_ = Eigen::Matrix3d::Identity() + (2.0 * _q_hat * _q_hat) + (2.0 * _quat_normalized(0) * _q_hat);
+
+    return(rotation_);
+} // end quat2rot()
+
+//// Convert quaternions to euler angles
+// references are the papers in the controller comments
+// Nice approximation of atan2: https://www.dsprelated.com/showarticle/1052.php
+Eigen::Matrix<double,1,3> quat2euler(const Eigen::Ref<const Eigen::Matrix<double,1,4>>& q_)
+{
+    Eigen::Matrix3d rotation_;
+    Eigen::Matrix<double,1,3> euler_;
+    euler_ << 0.0, 0.0, 0.0;
+
+    rotation_ = quat2rot(q_);
+    euler_(0) = asin(rotation_(1,2));   // roll
+    euler_(1) = atan2((-1.0*rotation_(0,2)) / cos(euler_(0)),
+                                   (rotation_(2,2) / cos(euler_(0))));
+    euler_(2) = atan2((-1.0*rotation_(1,0)) / cos(euler_(0)),
+                      (rotation_(1,1) / cos(euler_(0))));
+
+    return(euler_);
+
+} // end quat2euler()
+
+//// Derives angular velocity vector from euler angles
+Eigen::Matrix<double,1,3> derive_ang_velocity(const Eigen::Ref<const Eigen::Matrix<double,1,3>>& e_)
+{
+    Eigen::Matrix<double,3,3> tfm_;             // transformation matrix
+
+    tfm_(0,0) = cos(e_(1)); tfm_(0,1) = 0.0; tfm_(0,2) = (-1.0*cos(e_(0))*sin(e_(1)));
+    tfm_(1,0) = 0.0; tfm_(1,1) = 1.0; tfm_(1,2) = sin(e_(0));
+    tfm_(2,0) = sin(e_(1)); tfm_(2,1) = 0.0; tfm_(2,2) = (cos(e_(0))*cos(e_(1)));
+
+    _derived_pqr_att = tfm_ * (e_ - _prev_derived_euler_att).transpose();       // angular velocity vector
+
+    return(_derived_pqr_att);
+
+} // end derive_ang_velocity()
+
+#pragma clang diagnostic pop
---
+++ include/HIL_quad_control.hpp
@@ -0,0 +1,153 @@
+// Header file for basic_quad_control.cpp
+//
+// By: Patrick Ledzian
+// Date: 01 October 2020
+//
+
+#ifndef SET_ROTOR_VEL_HPP
+#define SET_ROTOR_VEL_HPP
+
+#include <gazebo/gazebo.hh>
+#include <gazebo/gazebo_config.h>
+#include <gazebo/transport/transport.hh>
+#include <gazebo/msgs/msgs.hh>
+#include <gazebo/common/common.hh>
+#include <string.h>
+#include <cmath>
+#include <eigen3/Eigen/Eigen>          // we have options here: ignition (limited), xtensor (like numpy), eigen (standard?)
+//#include <eigen3/Eigen/StdVector>
+#include <iostream>
+#include <fstream>
+
+#include <gazebo/gazebo_client.hh>
+#include "../msgs/include/Float.pb.h"
+#include "../msgs/include/Wind.pb.h"
+#include "../msgs/include/CommandMotorSpeed.pb.h"
+#include "../msgs/include/local_poses_stamped.pb.h"
+
+// gazebo related variables
+gazebo::transport::NodePtr node_handle;
+typedef const boost::shared_ptr<const std_msgs::msgs::Float> MotorSpeedPtr;         // points to protobuf custom message "Float"
+typedef const boost::shared_ptr<const gazebo::msgs::LocalPosesStamped> ConstLocalPosesStampedPtr;
+
+// simulation time
+double sim_time = 0.0;
+double prev_sim_time = 0.0;
+double prev_sim_time_pos = 0.0;
+double prev_sim_time_att = 0.0;
+double sim_time_delta = 0.0;
+
+// simulation state
+int sim_state = 0;              // 0 - not armed, 1 - pre-armed, 2 - armed
+
+// Reference motor values that are published
+std_msgs::msgs::Float ref_motor_vel0;
+std_msgs::msgs::Float ref_motor_vel1;
+std_msgs::msgs::Float ref_motor_vel2;
+std_msgs::msgs::Float ref_motor_vel3;
+
+// Measured motor values
+double sensor_motor_vel0 = 0.0;
+double sensor_motor_vel1 = 0.0;
+double sensor_motor_vel2 = 0.0;
+double sensor_motor_vel3 = 0.0;
+
+// Previous position and orientation values
+double prev_sensor_rot_x = 0.0;
+double prev_sensor_rot_y = 0.0;
+double prev_sensor_rot_z = 0.0;
+double prev_sensor_rot_w = 0.0;
+
+// Actions; for the state machine
+std::string steps ("steps");
+std::string hover ("hover");
+std::string min_snap ("min_snap");
+
+// TODO: I could probably pull this directly from the SDF file, not via head, but maybe dynamically at program start
+
+// environmental variables
+double _gravity = 9.81;     // m/s^2
+
+// initialize physical properties of the vehicle
+double _mass = 1.5;          // kg
+double _hover_point = 665.0; // rad/s for one motor only
+double _motor_force_const = 8.54858e-06;
+Eigen::Matrix4d _motor_mapping((Eigen::Matrix4d() << 1.0, 0.5, 0.5, 1.0, 1.0, -0.5, 0.5, -1.0, 1.0, -0.5, -0.5, 1.0, 1.0, 0.5, -0.5, -1.0).finished());
+
+// Setpoints for step response trajectory
+bool _set_pt1 = 1;
+bool _set_pt2 = 0;
+bool _set_pt3 = 0;
+bool _set_pt4 = 0;
+
+// Minimum snap trajectory variables
+Eigen::MatrixXd _traj_setpoints(6, 3);      // positions to achieve
+Eigen::MatrixXd _ts(5,1);                   // one less x dimension as _trajectory
+Eigen::MatrixXd _coef(40, 3);               // 8*(m_-1) x 3; where m_ is the row dimension of _traj_setpoints; resized in optimizer
+double _total_traj_time;
+bool _is_optimized = 0;                           // 0 -- not yet optimized, 1 -- optimal coefficients found
+bool _start_traj = 0;                             // 0 -- not started,       1 -- started / in-progress
+double _traj_time = 0.0;                          // current trajectory time
+double _traj_start_time = 0.0;
+bool _traj_finished = 0;
+
+// Measured sensor values
+Eigen::Matrix<double,1,4> _sensor_quat((Eigen::Matrix<double,1,4>() << 1.0, 0.0, 0.0, 0.0).finished());
+Eigen::Matrix<double,1,3> _sensor_pos;
+
+// Previous values for derivations
+Eigen::Matrix<double,1,3> _prev_derived_euler_att;
+
+// Derived values from sensor measurments
+Eigen::Matrix<double,1,3> _prev_sensor_pos;
+Eigen::Matrix<double,1,3> _derived_lin_vel;
+Eigen::Matrix<double,1,3> _derived_euler_att;
+Eigen::Matrix<double,1,3> _derived_pqr_att;
+
+// Desired values to achieve
+Eigen::Matrix<double,1,3> _desired_pos;
+Eigen::Matrix<double,1,3> _desired_vel;
+Eigen::Matrix<double,1,3> _desired_acc;
+Eigen::Matrix<double,1,3> _desired_euler_att;
+Eigen::Matrix<double,1,3> _orig_desired_euler_att;
+Eigen::Array<double,1,3> _desired_pqr_att;
+Eigen::Matrix<double,1,4> _desired_thrust((Eigen::Matrix<double,1,4>() << 0.0, 0.0, 0.0, 0.0).finished());
+double _desired_tot_thrust_delta;
+
+// Helper variables
+Eigen::Matrix3d _q_hat((Eigen::Matrix3d() << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0).finished());
+Eigen::Matrix<double,1,4> _quat_normalized((Eigen::Matrix<double,1,4>() << 1.0, 0.0, 0.0, 0.0).finished());
+Eigen::Matrix<double,1,3> _final_att_deltas;
+
+// Controller gains
+static Eigen::Matrix<double,1,3> _Kp_pos;
+static Eigen::Matrix<double,1,3> _Kd_pos;
+static Eigen::Matrix<double,1,3> _Kp_ang;
+static Eigen::Matrix<double,1,3> _Kd_ang;
+
+// Function declarations
+void setpoint_trajectory();
+void test_ol_takeoff();
+void test_ol_land();
+void test_cl_takeoff();
+void derived_sensor_values();
+void initialize_variables();
+void basic_position_controller();
+void basic_attitude_controller();
+void minimum_snap_trajectory();
+void generate_ts();
+void min_snap_optimization();
+void basic_hover();
+
+// Measured motor speed values; callback functions
+void rotor0_cb(MotorSpeedPtr &rotor_vel);
+void rotor1_cb(MotorSpeedPtr &rotor_vel);
+void rotor2_cb(MotorSpeedPtr &rotor_vel);
+void rotor3_cb(MotorSpeedPtr &rotor_vel);
+
+// Helper functions
+Eigen::Matrix3d quat2rot(const Eigen::Ref<const Eigen::Matrix<double,1,4>>& q);
+Eigen::Matrix<double,1,3> quat2euler(const Eigen::Ref<const Eigen::Matrix<double,1,4>>& q);
+Eigen::Matrix<double,1,3> derive_ang_velocity(const Eigen::Ref<const Eigen::Matrix<double,1,3>>& e_);
+
+#endif // SET_ROTOR_VEL_HPP
