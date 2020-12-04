getting closer, I have the quad hovering with small overshoot however the pitch/roll goes unstable within 5 seconds. There is still something wrong with the coordinate frames in the computation, commanding +x pitches forward (good) but comes back oscillating and unstable, commanding +y rolls left (bad/opposite) and flops.

iris_platform_analysis/geo_pos_analysis.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import time
 
-from scipy.spatial.transform import Rotation as R
+# from scipy.spatial.transform import Rotation as R
 
 filename = "sim_test_data/testdata_cont.txt"
 
@@ -19,13 +19,12 @@ time_ref = list()
 actual_pos = list()
 desired_pos = list()
 pos_err = list()
-omegac_hat = list()					# matrix
+omegac_hat = list()					# matrix 			pull out -W23, W13, -W12 // maybe just plot them straight from the matrix
 omegac_hat_resize = list()
 omegac = list()
-rot_err = list() 					# matrix
-rot_err_resize = list()
+rot_err = list() 					
 ang_vel_err = list()
-omega_hat = list() 					# matrix
+omega_hat = list() 					# matrix 			pull out -W23, W13, -W12
 omega_hat_resize = list()
 omegac_dot = list()
 desired_thrust_mag = list()
@@ -49,17 +48,13 @@ desired_rotor_rates = list()
 def read_data_in():
 	with open(filename, 'r') as reader:
 		i = 0
-		line_cnt = 20
+		line_cnt = 18
 		for line in reader:
 			line = line.replace("\n", "")
 			if (i%line_cnt) == 0: 					# time
 				time_ref.append(float(line))
 			elif (i%line_cnt) == 1: 							# actual_pos
-				# print(line)
-				# print(format_list(line))
 				actual_pos.append(format_list(line))
-				# print(actual_pos)
-				# time.sleep(1.0)
 			elif (i%line_cnt) == 2: 							# desired_pos
 				desired_pos.append(format_list(line))
 			elif (i%line_cnt) == 3: 							# pos_err
@@ -73,28 +68,24 @@ def read_data_in():
 			elif (i%line_cnt) == 7: 							# omegac
 				omegac.append(format_list(line))
 			elif (i%line_cnt) == 8: 							# rot_err
-				rot_err.append(line)
-			elif (i%line_cnt) == 9:
-				rot_err.append(line)
-			elif (i%line_cnt) == 10:
-				rot_err.append(line)
-			elif (i%line_cnt) == 11:							# ang_vel_err
+				rot_err.append(format_list(line))
+			elif (i%line_cnt) == 9:								# ang_vel_err
 				ang_vel_err.append(format_list(line))
-			elif (i%line_cnt) == 12: 							# omega_hat
+			elif (i%line_cnt) == 10: 							# omega_hat
 				omega_hat.append(line)
-			elif (i%line_cnt) == 13:
+			elif (i%line_cnt) == 11:
 				omega_hat.append(line)
-			elif (i%line_cnt) == 14:
+			elif (i%line_cnt) == 12:
 				omega_hat.append(line)
-			elif (i%line_cnt) == 15: 							# omegac_dot
+			elif (i%line_cnt) == 13: 							# omegac_dot
 				omegac_dot.append(format_list(line))
-			elif (i%line_cnt) == 16: 							# desired_thrust_mag
+			elif (i%line_cnt) == 14: 							# desired_thrust_mag
 				desired_thrust_mag.append(format_list(line))
-			elif (i%line_cnt) == 17: 							# desired_moments
+			elif (i%line_cnt) == 15: 							# desired_moments
 				desired_moments.append(format_list(line))
-			elif (i%line_cnt) == 18: 							# desired_rotor_rates
+			elif (i%line_cnt) == 16: 							# desired_rotor_rates
 				desired_rotor_rates.append(format_list(line))
-			elif (i%line_cnt) == 19:
+			elif (i%line_cnt) == 17:
 				pass
 			else:
 				print("ERROR")
@@ -136,7 +127,6 @@ def resize_data_in():
 	global omegac_hat_resize
 	global omegac
 	global rot_err 					# matrix
-	global rot_err_resize
 	global ang_vel_err
 	global omega_hat 				# matrix
 	global omega_hat_resize
@@ -147,11 +137,13 @@ def resize_data_in():
 
 
 	omegac_hat_resize = format_matrix(omegac_hat)
-	rot_err_resize = format_matrix(rot_err)
 	omega_hat_resize = format_matrix(omega_hat)
 
-	min_size = min(len(time_ref), len(actual_pos), len(desired_pos), len(pos_err), len(omegac_hat_resize), len(rot_err_resize), len(ang_vel_err), 
+	min_size = min(len(time_ref), len(actual_pos), len(desired_pos), len(pos_err), len(omegac_hat_resize), len(ang_vel_err), 
 		len(omegac_hat_resize), len(omegac_dot), len(desired_thrust_mag), len(desired_moments), len(desired_rotor_rates))
+
+	if min_size == 0:
+		print("Invalid 'min_size' -- value is 0")
 
 	time_ref = time_ref[0:min_size]
 	time_ref = np.array(time_ref)[0:min_size]
@@ -171,10 +163,8 @@ def resize_data_in():
 	omegac = omegac[0:min_size]
 	omegac = np.array(omegac)[0:min_size]
 	
-	rot_err_resize = rot_err_resize[0:min_size] 									# matrix
-	rot_err_resize = np.array(rot_err_resize)[0:min_size]
-	# test = R.from_matrix(rot_err_resize[100])
-	# print(test)
+	rot_err = rot_err[0:min_size]
+	rot_err = np.array(rot_err)[0:min_size]
 
 	ang_vel_err = ang_vel_err[0:min_size]
 	ang_vel_err = np.array(ang_vel_err)[0:min_size,:]
@@ -200,12 +190,15 @@ def visualize_results():
 	plt.figure(200)
 
 	plt.subplot(1,2,1)
-	plt.plot(time_ref[:], desired_pos[:,0], label='Des x')
-	plt.plot(time_ref[:], desired_pos[:,1], label='Des y')
-	plt.plot(time_ref[:], desired_pos[:,2], label='Des z')
-	plt.plot(time_ref[:], actual_pos[:,0], label='Act x')
-	plt.plot(time_ref[:], actual_pos[:,1], label='Act y')
-	plt.plot(time_ref[:], actual_pos[:,2], label='Act z')	
+	# plt.plot(time_ref[:], desired_pos[:,0], label='Des x')
+	# plt.plot(time_ref[:], desired_pos[:,1], label='Des y')
+	# plt.plot(time_ref[:], desired_pos[:,2], label='Des z')
+	# plt.plot(time_ref[:], actual_pos[:,0], label='Act x')
+	# plt.plot(time_ref[:], actual_pos[:,1], label='Act y')
+	# plt.plot(time_ref[:], actual_pos[:,2], label='Act z')	
+	plt.plot(time_ref[:], pos_err[:,0], label='x error')
+	plt.plot(time_ref[:], pos_err[:,1], label='y error')
+	plt.plot(time_ref[:], pos_err[:,2], label='z error')
 
 	plt.xlabel('Sim time (s)')
 	plt.ylabel('Position (m)')
@@ -213,25 +206,24 @@ def visualize_results():
 	plt.legend()
 	plt.grid(True)
 
-	# # plot attitude data
-	# plt.subplot(1,2,2)
-	# plt.plot(time, desired_att[:,0], label='Des roll')
-	# plt.plot(time, desired_att[:,1], label='Des pitch')
-	# plt.plot(time, desired_att[:,2], label='Des yaw')
-	# plt.plot(time, actual_att[:,0], label='Act roll')
-	# plt.plot(time, actual_att[:,1], label='Act pitch')
-	# plt.plot(time, actual_att[:,2], label='Act yaw')	
+	# plot attitude data
+	plt.subplot(1,2,2)
+	plt.plot(time_ref[:], rot_err[:,0], label='Roll error')
+	plt.plot(time_ref[:], rot_err[:,1], label='Pitch error')
+	plt.plot(time_ref[:], rot_err[:,2], label='Yaw error')
+	# plt.plot(time_ref[:], actual_att[:,0], label='Act roll')
+	# plt.plot(time_ref[:], actual_att[:,1], label='Act pitch')
+	# plt.plot(time_ref[:], actual_att[:,2], label='Act yaw')	
 
-	# plt.xlabel('Sim time (s)')
-	# plt.ylabel('Degrees (rad)')
-	# plt.title("Attitude over time")
-	# plt.legend()
-	# plt.grid(True)
+	plt.xlabel('Sim time (s)')
+	plt.ylabel('Attitude Error (rad)')
+	plt.title("Attitude error over time")
+	plt.legend()
+	plt.grid(True)
 
 
 	plt.figure(300)
 	plt.subplot(1,2,1)
-	plt.plot(time_ref[:], desired_thrust_mag[:], label="thrust mag")
 	plt.plot(time_ref[:], desired_rotor_rates[:,1], label="motor 1")
 	plt.plot(time_ref[:], desired_rotor_rates[:,1], label="motor 2")
 	plt.plot(time_ref[:], desired_rotor_rates[:,2], label="motor 3")
@@ -243,16 +235,17 @@ def visualize_results():
 	plt.legend()
 	plt.grid(True)
 
-	# plt.subplot(1,2,2)
-	# plt.plot(time, attitude_deltas[:,0], label="roll delta")
-	# plt.plot(time, attitude_deltas[:,1], label="pitch delta")
-	# plt.plot(time, attitude_deltas[:,2], label="yaw delta")
+	plt.subplot(1,2,2)
+	plt.plot(time_ref[:], desired_thrust_mag[:], label="thrust mag")
+	plt.plot(time_ref[:], desired_moments[:,0], label="roll force")
+	plt.plot(time_ref[:], desired_moments[:,1], label="pitch force")
+	plt.plot(time_ref[:], desired_moments[:,2], label="yaw force")
 
-	# plt.xlabel('Sim time (s)')
-	# plt.ylabel('Attitude Delta (rad)')
-	# plt.title("Attitude deltas over time")
-	# plt.legend()
-	# plt.grid(True)
+	plt.xlabel('Sim time (s)')
+	plt.ylabel('Desired moment forces (N)')
+	plt.title("Desired moments over time")
+	plt.legend()
+	plt.grid(True)
 
 	plt.tight_layout()
 	plt.show()